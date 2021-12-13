a3920d9f73e6d9a3163ab70b1bce85897f2a6f6c
S-Mlangeni <smlangeni21@gmail.com>
2021-12-13 12:04:41 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 
-$.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/get.php"*/, {
+$.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Stock_Management_System/System_Backend/get.php"*/, {
         "selectedSortValue": $("#clinicSorting").val()
     }, function(returnedData, ourpoststatus) {
         $(".fas").css({
@@ -14,7 +14,7 @@ $.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://l
 
 $("#clinicSorting").change(function() {
     if ($("#clinicSorting").val() == "allClinics") {
-        $.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/get.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Stock_Management_System/System_Backend/get.php"*/, {
             "selectedSortValue": "allClinics"
         }, function(returnedData, ourpoststatus) {
             $(".fas").css({
@@ -26,7 +26,7 @@ $("#clinicSorting").change(function() {
         });
     };
     if ($("#clinicSorting").val() == "lowMedClinics") {
-        $.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/get.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/get.php"/*"http://localhost/Stock_Management_System/System_Backend/get.php"*/, {
             "selectedSortValue": "lowMedClinics"
         }, function(returnedData, ourpoststatus) {
             $(".fas").css({
@@ -43,7 +43,7 @@ function buttonSelector() {
     $("button[id='NevAdd']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/post.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Stock_Management_System/System_Backend/post.php"*/, {
             "medicationType": "NEVIRAPINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
@@ -56,7 +56,7 @@ function buttonSelector() {
     $("button[id='NevRemove']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/delete.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Stock_Management_System/System_Backend/delete.php"*/, {
             "medicationType": "NEVIRAPINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
@@ -69,7 +69,7 @@ function buttonSelector() {
     $("button[id='StavAdd']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/post.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Stock_Management_System/System_Backend/post.php"*/, {
             "medicationType": "STAVUDINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
@@ -82,7 +82,7 @@ function buttonSelector() {
     $("button[id='StavRemove']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/delete.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Stock_Management_System/System_Backend/delete.php"*/, {
             "medicationType": "STAVUDINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
@@ -95,7 +95,7 @@ function buttonSelector() {
     $("button[id='ZidAdd']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/post.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/post.php"/*"http://localhost/Stock_Management_System/System_Backend/post.php"*/, {
             "medicationType": "ZIDOTABINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
@@ -108,7 +108,7 @@ function buttonSelector() {
     $("button[id='ZidRemove']").click(function() {
         $(this).html("<i class='fas fa-spinner fa-spin fas-off'></i>");
         const clinicNumber = $(this).prop("name");
-        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Mezzanine_Assignment/Mezzanine_Backend/delete.php"*/, {
+        $.post("https://stock-management-system-server.herokuapp.com/delete.php"/*"http://localhost/Stock_Management_System/System_Backend/delete.php"*/, {
             "medicationType": "ZIDOTABINE",
             "clinicNumber": clinicNumber,
             "selectedSortValue": $("#clinicSorting").val()
